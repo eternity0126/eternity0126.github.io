7aff5f8627a887778a8b03a1dd60696d2aa83577
SJTU-Lyx <sjtulyx2003@sjtu.edu.cn>
2025-01-01 10:40:00 +0800

    Site updated: 2025-01-01 10:39:59

---
+++ js/butterfly.js
@@ -0,0 +1,51 @@
+function func() {
+  console.log('Butterfly.js loaded!');
+// 选择所有 mjx-script 元素
+const mjxScripts = document.querySelectorAll('mjx-script');
+// 遍历每个 mjx-script 元素
+mjxScripts.forEach(script => {
+  console.log(script.parentNode.tagName);
+  if (script.parentNode.tagName === 'MJX-MSUB') {
+    script.style.cssText = 'vertical-align: -0.2em !important';
+  }
+  else if (script.parentNode.tagName === 'MJX-MUNDEROVER') {
+    script.style.cssText = 'vertical-align: -0.3em !important';
+  }
+  // 选择 mjx-texatom 元素
+  script.childNodes.forEach(child => {
+    if (child.tagName === 'MJX-TEXATOM' || child.tagName === 'MJX-MI' || child.tagName === 'MJX-MN') {
+      // 设置字体大小
+      child.style.cssText = 'font-size: 70.7% !important';
+    }
+  })
+});
+const mjxUnder = document.querySelectorAll('mjx-under, mjx-over');
+mjxUnder.forEach(under => {
+  const texAtom = under.querySelector('mjx-texatom') || under.querySelector('mjx-mi') || under.querySelector('mjx-mn');
+  if (texAtom) {
+    if (texAtom.tagName === 'MJX-TEXATOM') {
+      texAtom.style.cssText = 'font-size: 70.7% !important; padding-left: 0.4em';
+    }
+    else
+    {
+      texAtom.style.cssText = 'font-size: 70.7% !important';
+    }
+  }
+});
+const mjxSub = document.querySelectorAll('mjx-msub, mjx-msup');
+mjxSub.forEach(sub => {
+  sub.style.cssText = 'font-size: 100% !important';
+});
+}
+
+var int = self.setInterval("clock()", 1000);
+function clock() {
+    if (document.querySelector("mjx-script")) {
+        func();
+        clearInterval(int);
+    }
+    else
+    {
+      console.log('Waiting for MathJax...');
+    }
+}
